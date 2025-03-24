317b0fc9d2a8129a320cddf8e6616baff81aa7cb
Omar Minaya <omiinaya@gmail.com>
2025-03-24 17:24:48 -0400

    testing kasm.

---
+++ ct/kasm.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/omiinaya/ProxmoxVED/refs/heads/testing/misc/build.func)
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: Omar Minaya
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://www.kasmweb.com/docs/1.10.0/install/single_server_install.html
+
+APP="Kasm"
+var_tags="kasm;workspaces;docker"
+var_cpu="2"
+var_ram="4096"
+var_disk="50"
+var_os="debian"
+var_version="12"
+var_unprivileged="1"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+    header_info
+    check_container_storage
+    check_container_resources
+    if [[ ! -d /opt/kasm ]]; then
+        msg_error "No ${APP} Installation Found!"
+        exit
+    fi
+    msg_info "Updating ${APP} LXC"
+    $STD apt-get update
+    $STD apt-get -y upgrade
+    msg_ok "Updated ${APP} LXC"
+    exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}https://${IP}:443${CL}"
