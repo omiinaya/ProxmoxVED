ec195545857a54acf5e01b96eb94b59362fb63a2
Omar Minaya <omiinaya@gmail.com>
2025-05-21 17:54:56 -0400

    testing lyrion

--- install/lyrion-install.sh
+++ install/lyrion-install.sh
@@ -13,12 +13,12 @@ setting_up_container
 network_check
 update_os
 
-#msg_info "Installing Lyrion Music Server"
-#DEB_URL="https://downloads.lms-community.org/LyrionMusicServer_v9.0.2/lyrionmusicserver_9.0.2_amd64.deb"
-#DEB_FILE="/tmp/lyrionmusicserver_9.0.2_amd64.deb"
-#curl -fsSL -o "$DEB_FILE" "$DEB_URL" 2>&1 | tee -a ~/lyrion-install.log
-#$STD apt install "$DEB_FILE" -y 2>&1 | tee -a ~/lyrion-install.log
-#msg_ok "Installed Lyrion Music Server"
+msg_info "Installing Lyrion Music Server"
+DEB_URL="https://downloads.lms-community.org/LyrionMusicServer_v9.0.2/lyrionmusicserver_9.0.2_amd64.deb"
+DEB_FILE="/tmp/lyrionmusicserver_9.0.2_amd64.deb"
+curl -fsSL -o "$DEB_FILE" "$DEB_URL" 2>&1 | tee -a ~/lyrion-install.log
+$STD apt install "$DEB_FILE" -y 2>&1 | tee -a ~/lyrion-install.log
+msg_ok "Installed Lyrion Music Server"
 
 motd_ssh
 customize
