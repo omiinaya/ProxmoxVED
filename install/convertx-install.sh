cd3113416d514daa1960126c4d62ea0da27fc648
Omar <omiinaya@gmail.com>
2025-05-22 23:25:26 -0400

    fixed no login issue

--- install/convertx-install.sh
+++ install/convertx-install.sh
@@ -23,7 +23,11 @@ $STD ln -sf /root/.bun/bin/bun /usr/local/bin/bun
 $STD git clone "https://github.com/C4illin/ConvertX.git" /opt/convertx
 cd /opt/convertx
 $STD bun install
+
+JWT_SECRET=$(openssl rand -base64 32 | tr -dc 'a-zA-Z0-9' | head -c 32)
 cat <<EOF >/opt/convertx/.env
+JWT_SECRET=$JWT_SECRET
+HTTP_ALLOWED=true
 PORT=3000
 EOF
 msg_ok "Installed ConvertX"
