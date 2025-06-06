2fd99527025cf606aa28e162d85e298ccbd5d34c
Omar Minaya <omiinaya@gmail.com>
2025-06-06 16:51:58 -0400

    .

--- ct/fogproject.sh
+++ ct/fogproject.sh
@@ -12,12 +12,20 @@ var_ram="${var_ram:-2048}"
 var_disk="${var_disk:-16}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-12}"
-var_unprivileged="${var_unprivileged:-1}"
+var_unprivileged="${var_unprivileged:-0}"
 
 header_info "$APP"
 variables
 color
 catch_errors
+
+# Check for NFS server support
+if [[ "${var_unprivileged}" == "1" ]]; then
+  msg_warn "Unprivileged LXC detected. FOGProject requires NFS server support, which is usually only available in privileged containers."
+  if ! grep -q nfs /proc/filesystems; then
+    msg_warn "NFS server kernel module not available. FOG imaging will fail unless NFS is enabled on the host and container."
+  fi
+fi
 
 function update_script() {
   header_info
