19095fe7602285cf28210ade02d888fc396c0789
Omar Minaya <omiinaya@gmail.com>
2025-06-15 18:14:27 -0400

    testing2

--- install/kasm-install.sh
+++ install/kasm-install.sh
@@ -19,7 +19,7 @@ curl -fsSL -o "/opt/kasm_release_${KASM_VERSION}.tar.gz" "https://kasm-static-co
 cd /opt
 tar -xf "kasm_release_${KASM_VERSION}.tar.gz"
 chmod +x /opt/kasm_release/install.sh
-printf 'y\ny\ny\n4\n' | bash /opt/kasm_release/install.sh
+printf 'y\ny\ny\n4\n' | bash /opt/kasm_release/install.sh > ~/kasm-install.output 2>&1
 grep -A 20 -i "credentials\|login\|password\|admin" ~/kasm-install.output | sed '1i Kasm-Workspaces-Credentials' >~/kasm.creds
 msg_ok "Installed Kasm Workspaces"
 
