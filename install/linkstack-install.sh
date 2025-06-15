4d5619217bf0852cd39a1c4e04e2d82791eaf426
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-06-15 20:42:50 +0200

    Update install/linkstack-install.sh

--- install/linkstack-install.sh
+++ install/linkstack-install.sh
@@ -23,11 +23,7 @@ $STD apt-get install -y \
     unzip
 msg_ok "Installed dependencies"
 
-PHP_VERSION=8.2
-PHP_MODULE=sqlite3,mysql,gd,curl,mbstring,zip,xml,bcmath,fileinfo
-PHP_APACHE=YES
-
-install_php
+PHP_VERSION="8.2" PHP_MODULE="sqlite3, mysql, fileinfo" PHP_APACHE="YES" install_php
 
 msg_info "Installing LinkStack"
 $STD a2enmod rewrite
