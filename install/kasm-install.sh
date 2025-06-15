c8031a7df7ff6af2bb0f3277785b0d365796cb2b
Omar Minaya <omiinaya@gmail.com>
2025-06-15 17:53:30 -0400

    .

--- install/kasm-install.sh
+++ install/kasm-install.sh
@@ -16,25 +16,17 @@ update_os
 msg_info "Installing Kasm Workspaces"
 KASM_VERSION=$(curl -fsSL 'https://www.kasmweb.com/downloads' | grep -o 'https://kasm-static-content.s3.amazonaws.com/kasm_release_[^"]*\.tar\.gz' | head -n 1 | sed -E 's/.*release_(.*)\.tar\.gz/\1/')
 curl -fsSL -o "/opt/kasm_release_${KASM_VERSION}.tar.gz" "https://kasm-static-content.s3.amazonaws.com/kasm_release_${KASM_VERSION}.tar.gz"
-cd /opt
-tar -xf "kasm_release_${KASM_VERSION}.tar.gz"
-chmod +x /opt/kasm_release/install.sh
+tar -xf "/opt/kasm_release_${KASM_VERSION}.tar.gz" -C /opt
 printf 'y\ny\ny\n4\n' | bash /opt/kasm_release/install.sh
+touch ~/kasm-install.output
 grep -A 20 -i "credentials\|login\|password\|admin" ~/kasm-install.output | sed '1i Kasm-Workspaces-Credentials' >~/kasm.creds
 msg_ok "Installed Kasm Workspaces"
 
 motd_ssh
 customize
 
-msg_info "Displaying Kasm Credentials"
-if [ -f ~/kasm.creds ]; then
-  cat ~/kasm.creds
-else
-  echo "Credentials file not found. Check ~/kasm-install.output for login information."
-fi
-
 msg_info "Cleaning up"
-$STD rm -f /opt/kasm_release_${KASM_VERSION}.tar.gz
+$STD rm -f "/opt/kasm_release_${KASM_VERSION}.tar.gz"
 $STD apt-get -y autoremove
 $STD apt-get -y autoclean
 msg_ok "Cleaned"
