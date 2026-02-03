0008b8d2845e69b193acfc041803287f59bfb455
Joerg Heinemann <heinemannj66@gmail.com>
2026-02-03 15:31:21 +0100

    Update install/step-ca-install.sh
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- install/step-ca-install.sh
+++ install/step-ca-install.sh
@@ -43,7 +43,7 @@ msg_ok "Authorized low port-binding capabilities"
 
 msg_info "Add a smallstep CA service user - Will only be used by systemd to manage the CA"
 $STD useradd --user-group --system --home $(step path) --shell /bin/false step
-msg_ok "Created smallstepp CA service user"
+msg_ok "Created smallstep CA service user"
 
 #
 # Quick & Dirty step-ca init options - to be improved via whiptail
